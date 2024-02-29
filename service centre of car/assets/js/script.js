86dddcdcc805c3c46817dc9c369210bbeb1f5d7e
B.Dhansai <150512590+dhansai098@users.noreply.github.com>
2024-02-29 19:30:51 +0530

    Add files via upload

---
+++ service centre of car/assets/js/script.js
@@ -0,0 +1,8 @@
+'use strict';
+const navbar = document.querySelector("[data-navbar]");
+const navToggler = document.querySelector("[data-nav-toggler]");
+
+navToggler.addEventListener("click", function () {
+  navbar.classList.toggle("active");
+  this.classList.toggle("active");
+});
